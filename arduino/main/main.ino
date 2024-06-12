13e1e96b0442df0d87d24978fa7b38aaa59fc733
b12901183 <161189253+b12901183@users.noreply.github.com>
2024-06-12 18:38:33 +0800

    update

--- arduino/main/main.ino
+++ arduino/main/main.ino
@@ -1,16 +1,24 @@
 #include <Servo.h>   //載入函式庫，這是內建的，不用安裝
 
+char val
 Servo servo1;
 Servo servo2;   // 建立SERVO物件
 
 void setup() {
+  Serial.begin(9600);
+  Serial.println("BTbegin");
+
   pinMode(3,OUTPUT);
   servo1.attach(4);  // 設定要將伺服馬達接到哪一個PIN腳
   servo2.attach(5);  // 設定要將伺服馬達接到哪一個PIN腳
 }
 
 void loop() { 
-  angle = 
+  if(Serial.available()){
+    val = Serial.read();
+    Serial1.print(val);
+  }
+  int angle = atoi(val);
   int hor, ver;
   hor = angle / 1000;
   ver = angle % 1000;
